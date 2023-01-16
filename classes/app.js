9b56998cd6a42135137529563d60e52c3bf5340e
SomeRandomNeko1 <114647669+SomeRandomNeko1@users.noreply.github.com>
2023-01-16 10:05:45 +0100

    add debugging

--- classes/app.js
+++ classes/app.js
@@ -1,28 +1,54 @@
-class Dino
-{
-
-    constructor(naam, vleeseter, leeftijd)
-    {
+class Dino {
+    constructor(naam, vleeseter, leeftijd) {
         this.naam = naam;
         this.vleeseter = vleeseter;
         this.leeftijd = leeftijd;
-    }   
-
+    }
 }
 
-class App
-{
-    runApplication()
-    {
+class plant {
+    constructor(naming, leeftijd, honger, leeft) {
+        this.naming = naming;
+        this.leeftijd = leeftijd;
+        this.honger = honger;
+        this.leeft = leeft;
 
 
-        
+        plant.leeftijd = true
+        plant.honger = true;
+        plant.leeft = false;
     }
 }
 
 
 
+
+class App {
+    runApplication() {
+        console.log("hello world!")
+        let appnaam = "Li Hong";
+        let versienummer = "2.2";
+        let versiedatum = "22-22-2222";
+        let autheur = "Li Hong";
+        let copyright = "LH";
+        let distributeur = "Lihong-inc";
+        let darkmode = true;
+
+        console.log(appnaam);
+        console.log(versienummer);
+        console.log(versiedatum);
+        console.log(autheur);
+        console.log(copyright);
+        console.log(distributeur);
+        console.log(darkmode);
+
+    }
+}
+let eetDino = new plant("Planten-Eter", 12, "veel honger", "nee");
+console.log(eetDino);
+let trex = new Dino("Trex", true, 10);
+console.log(trex);
 let app = new App();
 app.runApplication();
-let trex = new Dino("Trex",true,10);
-console.log(trex);
+console.log("Planten-eter eet de plant grass")
+console.log("Trex eet de dino Planten-eter")
